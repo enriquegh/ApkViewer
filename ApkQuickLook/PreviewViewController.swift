39cfae4c1a9ab744abe0a5ae4a07da761022e262
Enrique Gonzalez <enrique@saucelabs.com>
2021-01-01 18:58:46 -0800

    Add ApkQuickLook target and initial look

---
+++ ApkQuickLook/PreviewViewController.swift
@@ -0,0 +1,70 @@
+//
+//  PreviewViewController.swift
+//  ApkQuickLook
+//
+//  Created by Enrique Gonzalez on 1/1/21.
+//
+
+import Cocoa
+import Quartz
+import ApkParser
+
+class PreviewViewController: NSViewController, QLPreviewingController {
+    
+    
+    @IBOutlet var appName: NSTextField!
+    @IBOutlet var packageName: NSTextField!
+    @IBOutlet var appVersion: NSTextField!
+    @IBOutlet var targetSDK: NSTextField!
+    @IBOutlet var minSDK: NSTextField!
+    @IBOutlet var nativeCode: NSTextField!
+    @IBOutlet var permissions: NSTextField!
+
+
+    
+    override var nibName: NSNib.Name? {
+        return NSNib.Name("PreviewViewController")
+    }
+
+    override func loadView() {
+        super.loadView()
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+     * Implement this method and set QLSupportsSearchableItems to YES in the Info.plist of the extension if you support CoreSpotlight.
+     *
+    func preparePreviewOfSearchableItem(identifier: String, queryString: String?, completionHandler handler: @escaping (Error?) -> Void) {
+        // Perform any setup necessary in order to prepare the view.
+        
+        // Call the completion handler so Quick Look knows that the preview is fully loaded.
+        // Quick Look will display a loading spinner while the completion handler is not called.
+        handler(nil)
+    }
+     */
+    
+    func preparePreviewOfFile(at url: URL, completionHandler handler: @escaping (Error?) -> Void) {
+        
+        // Add the supported content types to the QLSupportedContentTypes array in the Info.plist of the extension.
+        
+        // Perform any setup necessary in order to prepare the view.
+        
+        // Call the completion handler so Quick Look knows that the preview is fully loaded.
+        // Quick Look will display a loading spinner while the completion handler is not called.
+        let parser = ApkParser(apkPath: url.path)
+        parser.load()
+        parser.parse()
+        let apk = parser.createApkModel()
+        self.appName.stringValue = apk.appName
+        self.packageName.stringValue = apk.packageName
+        self.appVersion.stringValue = apk.appVersion
+        self.targetSDK.stringValue = apk.targetSdkVersion
+        self.minSDK.stringValue = apk.minSdkVersion
+        self.nativeCode.stringValue = apk.nativeCode
+        self.permissions.stringValue = apk.permissions.joined(separator: ",")
+
+        
+        
+        handler(nil)
+    }
+}
